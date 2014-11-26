bca54a28b69f041e8b26fb5b7fe8eaa3824a7ca8
Jorge Valbuena <jorgevalbuena2@gmail.com>
2014-11-25 23:53:57 -0500

    Added play, prev, next buttons..

--- ViewController.m
+++ ViewController.m
@@ -7,10 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "SphereMenu.h"
+#import "ChameleonFramework/Chameleon.h"
 
-
-@interface ViewController ()
-
+@interface ViewController () <SphereMenuDelegate>
+@property (nonatomic) int counter;
 @end
 
 @implementation ViewController
@@ -25,17 +26,61 @@
     self.player.allowLandscapeMode = YES;
     self.player.forceBackToPortraitMode = YES;
     self.player.allowAutoResizingPlayerFrame = YES;
+    self.player.playsinline = YES;
     
     [self.player loadWithPlaylistId:@"PLEE58C6029A8A6ADE"];
     
     // adding to subview
     [self.view addSubview:self.player];
+        
+    NSArray *colors = [[NSArray alloc] initWithObjects:FlatGreen, FlatMint, nil];
+    
+    self.view.backgroundColor = [UIColor colorWithGradientStyle:UIGradientStyleTopToBottom withFrame:self.view.frame andColors:colors];
+    
+    UIImage *startImage = [UIImage imageNamed:@"start"];
+    UIImage *image1 = [UIImage imageNamed:@"Arrow-left"];
+    UIImage *image2 = [UIImage imageNamed:@"play"];
+    UIImage *image3 = [UIImage imageNamed:@"Arrow-right"];
+    NSArray *images = @[image1, image2, image3];
+    SphereMenu *sphereMenu = [[SphereMenu alloc] initWithStartPoint:CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height-120)
+                                                         startImage:startImage
+                                                      submenuImages:images];
+    sphereMenu.delegate = self;
+    [self.view addSubview:sphereMenu];
     
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)sphereDidSelected:(int)index
+{
+    NSLog(@"sphere %d selected", index);
+    
+    if(index == 1) {
+        if(self.counter == 0) {
+            [self.player playVideo];
+            self.counter = 1;
+        }
+        else {
+            [self.player pauseVideo];
+            self.counter = 0;
+        }
+    }
+    else if(index == 0) {
+        [self.player previousVideo];
+    }
+    else {
+        [self.player nextVideo];
+    }
+    
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
 }
 
 @end
